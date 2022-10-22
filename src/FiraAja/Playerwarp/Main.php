fc5b63660c946472bb1ac63886220ab7b8998be4
AraaCuteUwU <raihanxstorm14@gmail.com>
2022-10-23 07:14:08 +0800

    Fix teleport to warp

--- src/FiraAja/Playerwarp/Main.php
+++ src/FiraAja/Playerwarp/Main.php
@@ -2,18 +2,19 @@
 
 namespace FiraAja\Playerwarp;
 
+use JsonException;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\event\Listener;
 use pocketmine\world\Position;
-use pocketmine\math\Vector3;
 use pocketmine\player\Player;
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\Config;
-use pocketmine\utils\TextFormat;
 
 class Main extends PluginBase implements Listener {
-    public $pwarp;
+
+    /** @var Config $pwarp */
+    private Config $pwarp;
 
     public function onEnable(): void
     {
@@ -22,6 +23,14 @@ class Main extends PluginBase implements Listener {
         $this->pwarp = new Config($this->getDataFolder() . "pwarps.yml", Config::YAML, array());
     }
 
+    /**
+     * @param CommandSender $sender
+     * @param Command $command
+     * @param string $label
+     * @param array $args
+     * @return bool
+     * @throws JsonException
+     */
     public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool
     {
         $name = strtolower($sender->getName());
@@ -97,40 +106,9 @@ class Main extends PluginBase implements Listener {
                     $sender->sendMessage("World: " . $world);
                     $sender->sendMessage("--- PlayerWarp Info ---");
                     return true;
-                case "newpos":
+                case "teleport":
+                case "tp":
                     $pwarpname = $args[1];
-                    if(!$this->pwarp->exists($pwarpname)) {
-                        $sender->sendMessage("§cERROR: §fThere is no playerwarp with the name: §b" . $pwarpname . "§f!");
-                        return true;
-                    }
-                    if($this->pwarp->getNested($pwarpname.".owner") == $sender->getName()) {
-                        $sender->sendMessage("§cYou can't edit this warp, because it's not yours!");
-                        return true;
-                    }
-                    $x = $sender->getPosition()->getX();
-                    $y = $sender->getPosition()->getY();
-                    $z = $sender->getPosition()->getZ();
-                    $world = $sender->getWorld()->getDisplayName();
-                    $this->pwarp->setNested($pwarpname. ".x", $x);
-                    $this->pwarp->setNested($pwarpname . ".y", $y);
-                    $this->pwarp->setNested($pwarpname.".z", $z);
-                    $this->pwarp->setNested($pwarpname.".world", $world);
-                    $this->pwarp->save();
-                    $this->pwarp->reload();
-                    $sender->sendMessage("§8[§aPlayerwarp§8] §fNew position has been set.");
-                    return true;
-                case "list":
-                    $pwarplist = array();
-                    foreach ($this->pwarp->getAll(true) as $pwarp) {
-                         array_push($pwarplist, $pwarp);
-                    }
-                    $sender->sendMessage("§bRegistered playerwarp:");
-                    $sender->sendMessage(implode(", ", $pwarplist));
-                    return true;
-
-
-                default:
-                    $pwarpname = $args[0];
                     if(!$this->pwarp->exists($pwarpname)) {
                         $sender->sendMessage("§cERROR: §fThere is no pwarp with the name " . $pwarpname . "!");
                         return true;
@@ -142,14 +120,30 @@ class Main extends PluginBase implements Listener {
                     $sender->teleport(new Position($x, $y, $z, $world));
                     $sender->sendMessage("§8[§aPlayerwarp§8] §fYou've been successfully teleported to the playerwarp: §b" . $pwarpname . "§f!");
                     return true;
+                case "list":
+                    $pwarplist = array();
+                    foreach ($this->pwarp->getAll(true) as $pwarp) {
+                         $pwarplist[] = $pwarp;
+                    }
+                    $sender->sendMessage("§bRegistered playerwarp:");
+                    $sender->sendMessage(implode(", ", $pwarplist));
+                    return true;
+
+                default:
+                    $sender->sendMessage("§cWRONG USAGE: §fRun /pwarp help to get help about playerwarp.");
+                    return true;
             }
         }
         return true;
     }
 
+    /**
+     * @param Player $player
+     * @return void
+     */
     public function getHelp(Player $player) {
         $player->sendMessage("--- Playerwarp Help ---");
-         $player->sendMessage("§f/pwarp <pwarp-name> §7- teleport to an pwarp");
+         $player->sendMessage("§f/pwarp teleport <pwarp-name> §7- teleport to an pwarp");
          $player->sendMessage("§f/pwarp create <pwarpname> §7- create an pwarp");
          $player->sendMessage("§f/pwarp delete <pwarpname> §7- delete an pwarp");
          $player->sendMessage("§f/pwarp newpos <pwarp-name> §7- change position of an pwarp");
